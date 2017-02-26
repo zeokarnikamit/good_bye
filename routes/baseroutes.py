43a9a602a3f122c591e7a407fae8e3c61d763930
karnikamit <karnikamit46@gmail.com>
2017-02-26 19:59:09 +0530

    get remote address

--- routes/baseroutes.py
+++ routes/baseroutes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = "karnikamit"
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,8 +13,8 @@ def init():
 
 @app.route('/click_me')
 def click_me():
-    import pdb; pdb.set_trace()
-    return ''
+    remote_ip = request.remote_addr
+    return 'Bye ZeOmega'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9001, debug=True)
